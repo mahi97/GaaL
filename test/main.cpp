cea96c4c3ff8df4b5b521fa92f63492a74d1c3ff
Mohammad Mahdi Rahimi <mohammadmahdi76@gmail.com>
2020-06-13 09:02:47 +0430

    write new test

--- test/main.cpp
+++ test/main.cpp
@@ -1,26 +1,27 @@
 //
 // Created by Mahi on 6/12/20.
 //
-#include "sslworld.h"
+#include "sslworld.hpp"
 #include "iostream"
+#include <vector>
 
 int main() {
-    auto* ssl = new SSLWorld(0.016);
-    bool done = true;
-    Action actions[6]{};
-    for (int i = 0; i < 6; i++) {
-        actions[i].robot_id = i % 3;
-        actions[i].team_id = (i <  3? 0 : 1);
-        actions[i].use_wheel = false;
-        actions[i].vel[0] = actions[i].vel[0] = actions[i].vel[0] = 100;
-    }
-
-    while (done) {
+    auto* ssl = new SSLWorld();
+    bool done = false;
+    ssl->reset();
+    std::vector<Action*> actions;
+    actions.clear();
+    auto action = new Action(0,0);
+    actions.push_back(action);
+    while (!done) {
         auto ob = ssl->getObservation(-1);
         auto& b = ob->robots[0][0];
         std::cout << b[0] << " " << b[1] << " " << b[2] << std::endl;
-        ssl->act(actions, 5);
+        action->vel[0] = action->vel[1] = action->vel[2] = ssl->framenum;
+        ssl->act(actions);
         ssl->step();
+        if (ssl->framenum > 100)
+            ssl->reset();
     }
     return 0;
 }
